79e206c6d7a7152554f8fa1a956a0380f0d28443
danielo515 <rdanielo@gmail.com>
2015-01-25 09:59:09 +0100

    Added documentation and examples

--- tiddlers/macros/substract.js
+++ tiddlers/macros/substract.js
@@ -1,10 +1,10 @@
 /*\
-title: $:/widgets/danielo515/substract.js
+title: $:/widgets/danielo515/subtract.js
 type: application/javascript
 module-type: macro
 
 This macro returns the value of the specified field of the specified tiddler
-substracting the provided value
+subtracting the provided value
 
 \*/
 (function(){
@@ -17,7 +17,7 @@ substracting the provided value
   Information about this macro
   */
 
-  exports.name = "substract";
+  exports.name = "subtract";
 
   exports.params = [
   {name: "tiddler"},
